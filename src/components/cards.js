202dd72ac465f888269d36a4a2fc2216a86c3d23
sashank_saya <sashanksaya@gmail.com>
2023-09-17 11:04:18 +0530

    Last card

--- src/components/cards.js
+++ src/components/cards.js
@@ -40,26 +40,26 @@ import background from "../images/background.jpg";
 
 function Card() {
   return (
-    <Container>
-      <Row>
+    <Container fluid>
+      <Row className="d-flex justify-content-around">
         <Col>
           <Service
             title="Service"
-            description="Lorem Ipsum"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel rutrum eros, tempus congue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis quis lacinia diam. Nullam viverra bibendum sem, in maximus risus rutrum vel. Sed pharetra lacus eget suscipit vestibulum."
             background={background}
           />
         </Col>
         <Col>
           <Service
             title="Service"
-            description="Lorem Ipsum"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel rutrum eros, tempus congue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis quis lacinia diam. Nullam viverra bibendum sem, in maximus risus rutrum vel. Sed pharetra lacus eget suscipit vestibulum."
             background={background}
           />
         </Col>
         <Col>
           <Service
             title="Service"
-            description="Lorem Ipsum"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel rutrum eros, tempus congue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis quis lacinia diam. Nullam viverra bibendum sem, in maximus risus rutrum vel. Sed pharetra lacus eget suscipit vestibulum."
             background={background}
           />
         </Col>
