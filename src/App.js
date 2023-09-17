80a4ba54c8f530f646036132dc2170f25fae4988
shivaprasad-s-shetty <143221964+shivaprasad-s-shetty@users.noreply.github.com>
2023-09-17 13:33:54 +0530

    major changes here (#17)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
+// App.jsx
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import TopVideo from "./components/topvideo";
 import Card from "./components/cards";
+import background from './images/background.jpg';
+// import Crousel from "./components/crousel";
 
 function App() {
-  // const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // const toggleMenu = () => {
-  //   setIsMenuOpen(!isMenuOpen);
-  // };
   return (
     <div className="App">
       <Navbar />
       <main>
         <TopVideo />
-        <Card />
+        {/* <Crousel /> */}
+        <div>
+          <h1 className="servicesheading">Services</h1>
+          <div className="cards-container">
+            <Card
+              title="Service 1"
+              description="Description of Service 1 goes here."
+              background={background}
+            />
+            <Card
+              title="Service 2"
+              description="Description of Service 2 goes here."
+              background={background}
+            />
+            <Card
+              title="Service 3"
+              description="Description of Service 3 goes here."
+              background={background}
+            />
+            <Card
+              title="Service 3"
+              description="Description of Service 3 goes here."
+              background={background}
+            />
+          </div>
+        </div>
+
       </main>
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,42 @@
+// import React, { useState } from 'react';
+// import './navbar.css';
+// import logo from '../images/logo.jpeg';
+// import Menu from './menu';
+
+
+// function Navbar() {
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsMenuOpen(!isMenuOpen);
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <nav className={`navbar ${isMenuOpen ? 'menu-open' : ''}`}>
+//           <div className="container">
+//             <div className="logo">
+//               <img src={logo} alt="Logo" className="small-logo" />
+//             </div>
+//             <div className={`nav-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+//               <div className="bar"></div>
+//               <div className="bar"></div>
+//               <div className="bar"></div>
+//             </div>
+//           </div>
+//         </nav>
+//         <Menu isOpen={isMenuOpen} onClose={toggleMenu} />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Navbar;
 import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../images/logo.jpeg';
 import Menu from './menu';
-
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,13 +50,17 @@ function Navbar() {
       <div>
         <nav className={`navbar ${isMenuOpen ? 'menu-open' : ''}`}>
           <div className="container">
-            <div className="logo">
-              <img src={logo} alt="Logo" className="small-logo" />
+            <div className="navbar-left">
+              <div className="logo">
+                <img src={logo} alt="Logo" className="small-logo" />
+              </div>
             </div>
-            <div className={`nav-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
+            <div className="navbar-right">
+              <div className="nav-toggle" onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+              </div>
             </div>
           </div>
         </nav>
@@ -33,3 +71,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
