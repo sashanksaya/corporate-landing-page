9f9b1e5422377f3d9d142035d9939997a4d41dbb
Sashank Saya <sashanksaya@gmail.com>
2023-09-17 15:24:49 +0530

    Last card (#16)
    Co-authored-by: shivaprasad-s-shetty <143221964+shivaprasad-s-shetty@users.noreply.github.com>

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,15 +4,33 @@ import './cards.css'; // Import your CSS file
 
 function Card(props) {
   return (
-    <div className="card-container">
-      <div className="image-container">
-        <img src={props.background} alt="Background" />
-      </div>
-      <div className="card-content">
-        <h3 className="card-title">{props.title}</h3>
-        <p className="card-text">{props.description}</p>
-      </div>
-    </div>
+
+    <Container fluid>
+      <Row className="d-flex justify-content-around">
+        <Col>
+          <Service
+            title="Service"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel rutrum eros, tempus congue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis quis lacinia diam. Nullam viverra bibendum sem, in maximus risus rutrum vel. Sed pharetra lacus eget suscipit vestibulum."
+            background={background}
+          />
+        </Col>
+        <Col>
+          <Service
+            title="Service"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel rutrum eros, tempus congue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis quis lacinia diam. Nullam viverra bibendum sem, in maximus risus rutrum vel. Sed pharetra lacus eget suscipit vestibulum."
+            background={background}
+          />
+        </Col>
+        <Col>
+          <Service
+            title="Service"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel rutrum eros, tempus congue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis quis lacinia diam. Nullam viverra bibendum sem, in maximus risus rutrum vel. Sed pharetra lacus eget suscipit vestibulum."
+            background={background}
+          />
+        </Col>
+      </Row>
+    </Container>
+
   );
 }
 
