a1dfc840d92a3810211c75ff75dc46213d6703ed
Sashank Saya <sashanksaya@gmail.com>
2023-09-18 15:02:46 +0530

    preFinal (#21)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// App.jsx
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import TopVideo from "./components/topvideo";
 import Card from "./components/cards";
-import Footer from "./components/foooooooooot";
-// import Crousel from "./components/crousel";
+import Footer from "./components/footer";
+import Scroll from "./components/scrolling";
+import Upbutton from "./components/upbutton";
+import TeamSection from "./components/teamsection";
 
 function App() {
   return (
@@ -14,14 +15,20 @@ function App() {
       <main>
         <TopVideo />
         {/* <Crousel /> */}
-        <div>
-          <h1 className="servicesheading">Services</h1>
-          <div className="cards-container">
-            <Card />
-          </div>
-          < Footer />
+        <h1 className="servicesheading">Services</h1>
+        <div className="cards-container">
+          <Card />
         </div>
-
+        <Upbutton />
+        <div className="team" id="team">
+          <h1 className="us">Our Team</h1>
+          <TeamSection />
+        </div>
+        <div className="scrolling">
+          <h1 className="clientsheading">Portfolio</h1>
+          <Scroll />
+        </div>
+        <Footer />
       </main>
     </div>
   );
