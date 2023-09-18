06bc422b8f0fa349a56e22eb13d838d524c33b1d
Shivaprasad S Shetty <shivaprasadshetty112@gmail.com>
2023-09-18 22:00:21 +0530

    images added

---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './login.css'; // Import your CSS file for styling
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        {/* Add your login form fields here */}
+        <button className="signup-button">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
