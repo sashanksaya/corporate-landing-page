8e8447eb434cdb566530d1fc356116eef21ab7d9
sashank_saya <sashanksaya@gmail.com>
2023-09-21 00:48:08 +0530

    backend

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./footer.css";
 import Corpify from "../images/corpify2.png";
+import axios from "axios";
 
 const Footer = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.length === 0) {
+      alert("Please enter your name");
+    } else if (email.length === 0) {
+      alert("Please enter your email");
+    } else if (phone.length === 0) {
+      alert("Please enter your phone number");
+    } else {
+      const url = "api.php";
+      let fdata = new FormData();
+      fdata.append("name", name);
+      fdata.append("email", email);
+      fdata.append("phone", phone);
+
+      axios
+        .post(url, fdata)
+        .then((response) => alert(response.data))
+        .catch((error) => alert(error));
+
+      // Here, you can send the form data to your server or perform other actions
+    }
+  };
+
   return (
     <footer className="footer">
-      {/* <div className="container"> */}
       <div className="row">
         <div className="col-md-4">
           <a href="/" className="d-flex align-items-center p-0 text-dark">
@@ -16,7 +44,6 @@ const Footer = () => {
             We are creating High-Quality Resources and tools to aid developers
             during the development of their projects
           </p>
-          {/* Social media icons/buttons */}
         </div>
         <div className="col-md-2">
           <h5 className="mb-4" style={{ fontWeight: "600" }}>
@@ -53,7 +80,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-md-4-a">
+        <div className="col-md-4">
           <h5 className="mb-4" style={{ fontWeight: "600" }}>
             Products
           </h5>
@@ -70,23 +97,44 @@ const Footer = () => {
           </ul>
         </div>
       </div>
+
       <div className="input">
         <h3>Enter your contact info</h3>
-        <label>Name </label>
-        <input type="text" />
-        <br></br>
-        <br></br>
-        <label>Email </label>
-        <input type="text" />
-        <br></br>
-        <br></br>
-        <label>Phone-no </label>
-        <input type="text" />
-        <br></br>
-        <br></br>
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
+        <form onSubmit={handleSubmit} method="POST">
+          <label htmlFor="name">Name </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="email">Email </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="phone">Phone-no </label>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <br></br>
+          <br></br>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
       </div>
       <div className="footer-bottom">
         <div className="container">
